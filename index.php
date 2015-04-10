c81766378fccd701aeef081a269f64c1f14dec1a
pablo <pablo@adianti.com.br>
2015-04-09 23:05:55 -0300

    Adicionado Bootstrap Datagrid Wrapper

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
 date_default_timezone_set('America/Sao_Paulo');
 
+// Lib loader
 require_once 'Lib/Livro/Core/ClassLoader.php';
 $al= new Livro\Core\ClassLoader;
 $al->addNamespace('Livro', 'Lib/Livro');
+$al->addNamespace('Bootstrap', 'Lib/Bootstrap');
 $al->register();
 
+// App loader
 require_once 'Lib/Livro/Core/AppLoader.php';
 $al= new Livro\Core\AppLoader;
 $al->addDirectory('App/Control');
