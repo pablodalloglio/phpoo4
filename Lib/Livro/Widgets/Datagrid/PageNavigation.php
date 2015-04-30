c47e3a21cd3b95a30e8cd38d7578b4479d63d878
pablo <pablo@adianti.com.br>
2015-04-30 19:12:59 -0300

    Implementado PageNav

--- Lib/Livro/Widgets/Datagrid/PageNavigation.php
+++ Lib/Livro/Widgets/Datagrid/PageNavigation.php
@@ -1,6 +1,8 @@
 <?php
 namespace Livro\Widgets\Datagrid;
 
+use Livro\Control\Action;
+
 /**
  * Representa a paginação de uma datagrid
  * @author Pablo Dall'Oglio
@@ -12,55 +14,36 @@ class PageNavigation
     private $currentPage;
     private $totalRecords;
     
-    /**
-     * método ()
-     *
-     * @param  $xxxx    = xxxx
-     */
+    public function __construct()
+    {
+        $this->pageSize = 10;
+    }
+    
     function setAction(Action $action)
     {
         $this->action = $action;
     }
     
-    /**
-     * método ()
-     *
-     * @param  $xxxx    = xxxx
-     */
     function setPageSize($pageSize)
     {
         $this->pageSize = $pageSize;
     }
     
-    /**
-     * método ()
-     *
-     * @param  $xxxx    = xxxx
-     */
     function setCurrentPage($currentPage)
     {
         $this->currentPage = $currentPage;
     }
     
-    /**
-     * método ()
-     *
-     * @param  $xxxx    = xxxx
-     */
     function setTotalRecords($totalRecords)
     {
         $this->totalRecords = $totalRecords;
     }
     
-    /**
-     * método ()
-     *
-     * @param  $xxxx    = xxxx
-     */
     function show()
     {
         $pages = ceil($this->totalRecords / $this->pageSize);
         
+        echo '<ul class="pagination">';
         for ($n=1; $n <= $pages; $n++)
         {
             $offset = ($n -1) * $this->pageSize;
@@ -70,8 +53,13 @@ class PageNavigation
             $action->setParameter('page',   $n);
             
             $url = $action->serialize();
-            $label = ($this->currentPage == $n) ? "<u><b>$n</b></u>" : $n;
-            echo "<a href='$url'>{$label}</a>&nbsp;&nbsp;";
+            $class = ($this->currentPage == $n) ? 'active' : '';
+            
+            echo "<li class='{$class}'>";
+            echo "<a href='$url'>{$n}</a>&nbsp;&nbsp;";
+            echo '</li>';
+            
         }
+        echo '</ul>';
     }
 }
