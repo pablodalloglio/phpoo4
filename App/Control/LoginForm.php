18c82992ead58ff6f3f36454bfdb06de9463ac71
Pablo Dall Oglio <pablo@dalloglio.net>
2015-04-30 18:52:02 -0300

    Controle de login

---
+++ App/Control/LoginForm.php
@@ -0,0 +1,69 @@
+<?php
+use Livro\Control\Page;
+use Livro\Control\Action;
+use Livro\Widgets\Form\Form;
+use Livro\Widgets\Form\Entry;
+use Livro\Widgets\Form\Password;
+use Livro\Widgets\Form\Button;
+use Livro\Widgets\Wrapper\DatagridWrapper;
+use Livro\Widgets\Wrapper\FormWrapper;
+use Livro\Widgets\Container\Panel;
+
+use Livro\Session\Session;
+
+/**
+ * Formulário de Login
+ */
+class LoginForm extends Page
+{
+    private $form; // formulário
+    
+    /**
+     * Construtor da página
+     */
+    function __construct()
+    {
+        parent::__construct();
+
+        // instancia um formulário
+        $this->form = new FormWrapper(new Form('form_login'));
+        
+        $login      = new Entry('login');
+        $password   = new Password('password');
+        
+        $login->placeholder    = 'admin';
+        $password->placeholder = 'admin';
+        
+        $this->form->addField('Login',    $login,    200);
+        $this->form->addField('Senha',    $password, 200);
+        $this->form->addAction('Login', new Action(array($this, 'onLogin')));
+        
+        $panel = new Panel('Login');
+        $panel->add($this->form);
+        
+        // adiciona o formulário na página
+        parent::add($panel);
+    }
+    
+    /**
+     * Login
+     */
+    public function onLogin($param)
+    {
+        $data = $this->form->getData();
+        if ($data->login == 'admin' AND $data->password == 'admin')
+        {
+            Session::setValue('logged', TRUE);
+            echo "<script language='JavaScript'> window.location = 'index.php'; </script>";
+        }
+    }
+    
+    /**
+     * Logout
+     */
+    public function onLogout($param)
+    {
+        Session::setValue('logged', FALSE);
+        echo "<script language='JavaScript'> window.location = 'index.php'; </script>";
+    }
+}
