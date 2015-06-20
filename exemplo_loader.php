511b3606b7db1a6aa8bb4daa85931b8cc24d82a1
Pablo Dall Oglio <pablo@dalloglio.net>
2015-06-20 18:25:45 -0300

    exemplo loader

---
+++ exemplo_loader.php
@@ -0,0 +1,10 @@
+<?php
+// Lib loader
+require_once 'Lib/Livro/Core/ClassLoader.php';
+$al= new Livro\Core\ClassLoader;
+$al->addNamespace('Livro', 'Lib/Livro');
+$al->register();
+
+use Livro\Database\Connection;
+$obj1 = Connection::open('livro');
+var_dump($obj1);
