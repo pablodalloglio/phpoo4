321fe89936cbec6f14ddbc083a122fc267085224
Pablo Dall Oglio <pablo@dalloglio.net>
2018-03-10 16:29:37 -0300

    Gráficos

---
+++ App/Control/VendasMesChart.php
@@ -0,0 +1,52 @@
+<?php
+use Livro\Control\Page;
+use Livro\Widgets\Dialog\Message;
+use Livro\Database\Transaction;
+use Livro\Widgets\Container\Panel;
+
+/**
+ * Vendas por mês
+ */
+class VendasMesChart extends Page
+{
+    /**
+     * método construtor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        
+        require_once 'Lib/Twig/Autoloader.php';
+        Twig_Autoloader::register();
+        $loader = new Twig_Loader_Filesystem('App/Resources');
+        $twig = new Twig_Environment($loader);
+        $template = $twig->loadTemplate('vendas_mes.html');
+        
+        try
+        {
+            // inicia transação com o banco 'livro'
+            Transaction::open('livro');
+            $vendas = Venda::getVendasMes();
+            Transaction::close(); // finaliza a transação
+        }
+        catch (Exception $e)
+        {
+            new Message('error', $e->getMessage());
+            Transaction::rollback();
+        }
+        
+        // vetor de parâmetros para o template
+        $replaces = array();
+        $replaces['title'] = 'Vendas por mês';
+        $replaces['labels'] = json_encode(array_keys($vendas));
+        $replaces['data']  = json_encode(array_values($vendas));
+        
+        $content = $template->render($replaces);
+        
+        // cria um painél para conter o formulário
+        $panel = new Panel('Vendas/mês');
+        $panel->add($content);
+        
+        parent::add($panel);
+    }
+}
--- App/Model/Venda.php
+++ App/Model/Venda.php
@@ -1,4 +1,5 @@
 <?php
+use Livro\Database\Transaction;
 use Livro\Database\Record;
 use Livro\Database\Repository;
 use Livro\Database\Criteria;
@@ -63,7 +64,7 @@ class Venda extends Record
         }
     }
     
-    /*
+    /**
      * Retorna os itens da venda
      */
     public function get_itens()
@@ -77,5 +78,59 @@ class Venda extends Record
         $this->itens = $repositorio->load($criterio);
         // retorna os itens
         return $this->itens;
+    }
+    
+    /**
+     * Retorna vendas por mes
+     */
+    public static function getVendasMes()
+    {
+        $meses = array();
+        $meses[1] = 'Janeiro';
+        $meses[2] = 'Fevereiro';
+        $meses[3] = 'Março';
+        $meses[4] = 'Abril';
+        $meses[5] = 'Maio';
+        $meses[6] = 'Junho';
+        $meses[7] = 'Julho';
+        $meses[8] = 'Agosto';
+        $meses[9] = 'Setembro';
+        $meses[10] = 'Outubro';
+        $meses[11] = 'Novembro';
+        $meses[12] = 'Dezembro';
+        
+        $conn = Transaction::get();
+        $result = $conn->query("select strftime('%m', data_venda) as mes, sum(valor_final) as valor from venda group by 1");
+        
+        $dataset = [];
+        foreach ($result as $row)
+        {
+            $mes = $meses[ (int) $row['mes'] ];
+            $dataset[ $mes ] = $row['valor'];
+        }
+        
+        return $dataset;
+    }
+    
+    /**
+     * Retorna vendas por mes
+     */
+    public static function getVendasTipo()
+    {
+        $conn = Transaction::get();
+        $result = $conn->query("  SELECT tipo.nome as tipo, sum(item_venda.quantidade*item_venda.preco) as total
+                                    FROM venda, item_venda, produto, tipo
+                                   WHERE venda.id = item_venda.id_venda 
+                                     AND item_venda.id_produto = produto.id
+                                     AND produto.id_tipo = tipo.id
+                                GROUP BY 1");
+        
+        $dataset = [];
+        foreach ($result as $row)
+        {
+            $dataset[ $row['tipo'] ] = $row['total'];
+        }
+        
+        return $dataset;
     }
 }
