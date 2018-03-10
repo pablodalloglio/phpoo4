20172b2508e066e33b1ebfe44255879340578f9c
pablo <pablo@adianti.com.br>
2018-03-10 10:56:16 -0300

    Lista de produtos com QRCode e Barcode

---
+++ App/Control/ProdutosReport.php
@@ -0,0 +1,82 @@
+<?php
+use Livro\Control\Page;
+use Livro\Control\Action;
+use Livro\Widgets\Form\Form;
+use Livro\Widgets\Form\Entry;
+use Livro\Widgets\Form\Date;
+use Livro\Widgets\Dialog\Message;
+use Livro\Database\Transaction;
+use Livro\Database\Repository;
+use Livro\Database\Criteria;
+use Livro\Database\Filter;
+
+use Livro\Widgets\Wrapper\FormWrapper;
+use Livro\Widgets\Container\Panel;
+
+/**
+ * Relatório de vendas
+ */
+class ProdutosReport extends Page
+{
+    private $form;   // formulário de entrada
+
+    /**
+     * método construtor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        
+        require_once 'Lib/Twig/Autoloader.php';
+        Twig_Autoloader::register();
+        
+        $loader = new Twig_Loader_Filesystem('App/Resources');
+        $twig = new Twig_Environment($loader);
+        $template = $twig->loadTemplate('produtos_report.html');
+        
+        // vetor de parâmetros para o template
+        $replaces = array();
+        
+        // gerador Barcode em HTML
+        $generator = new Picqer\Barcode\BarcodeGeneratorHTML();
+        
+        // gerador QRCode em SVG
+        $renderer = new \BaconQrCode\Renderer\Image\Svg();
+        $renderer->setHeight(256);
+        $renderer->setWidth(256);
+        $renderer->setMargin(0);
+        $writer = new \BaconQrCode\Writer($renderer);
+        
+        try
+        {
+            // inicia transação com o banco 'livro'
+            Transaction::open('livro');
+            
+            $produtos = Produto::all();
+            foreach ($produtos as $produto)
+            {
+                $produto->barcode = $generator->getBarcode($produto->id, $generator::TYPE_CODE_128, 5, 100);
+                $produto->qrcode  = $writer->writeString($produto->id . ' ' . $produto->descricao);
+            }
+            $replaces['produtos'] = $produtos;
+            
+            // finaliza a transação
+            Transaction::close();
+        }
+        catch (Exception $e)
+        {
+            new Message('error', $e->getMessage());
+            Transaction::rollback();
+        }
+        
+        $content = $template->render($replaces);
+        
+        $title = 'Produtos';
+        
+        // cria um painél para conter o formulário
+        $panel = new Panel($title);
+        $panel->add($content);
+        
+        parent::add($panel);
+    }
+}
