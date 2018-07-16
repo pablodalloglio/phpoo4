e3753ca2172c51460347e52ec2a7488fa62cdb46
Pablo Dall Oglio <pablo@dalloglio.net>
2018-07-16 15:04:40 -0300

    index min version

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 date_default_timezone_set('America/Sao_Paulo');
 
+if (version_compare(PHP_VERSION, '7.0.0') == -1)
+{
+    die('A versão mínima do PHP para rodar esta aplicação é: 7.0.0');
+}
+
 // Lib loader
 require_once 'Lib/Livro/Core/ClassLoader.php';
 $al= new Livro\Core\ClassLoader;
