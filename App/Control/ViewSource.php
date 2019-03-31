59d9f722fd9ab22259cac246ace862436e883e1c
pablo <pablo@adianti.com.br>
2019-03-30 23:01:34 -0300

    melhorias visuais

--- App/Control/ViewSource.php
+++ App/Control/ViewSource.php
@@ -12,11 +12,19 @@ class ViewSource extends Page
     
     public function onView($param)
     {
+    
+        ini_set('highlight.comment', "#808080");
+        ini_set('highlight.default', "#FFFFFF");
+        ini_set('highlight.html',    "#C0C0C0");
+        ini_set('highlight.keyword', "#62d3ea");
+        ini_set('highlight.string',  "#FFC472");
+        
         $class = $param['source'];
         $file = "App/Control/{$class}.php";
         if (file_exists( $file ))
         {
             $panel = new Panel('Código-fonte: '. $class);
+            $panel->id = 'source-panel';
             $panel->add( highlight_file($file, TRUE) );
             
             parent::add($panel);
